c7a8f2c14dec14415e218410a50e563650ce59cc
Scott Strool <scott.strool@snyk.io>
2020-05-12 13:10:34 -0400

    Added user avatar bits

--- src/index.js
+++ src/index.js
@@ -5,19 +5,6 @@ const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
-
-// app.use((req, res, next) => {
-//   if (req.method === 'GET') {
-//     res.send('Get requests are disabled')
-//   } else {
-//     next()
-//   }
-// })
-
-// app.use((req, res, next) => {
-//   res.status(503).send('Site is under maintenance')
-// })
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -26,19 +13,3 @@ app.use(taskRouter)
 app.listen(port,() => {
   console.log('Server is up on port ' + port)
 })
-
-// const Task = require('./models/task')
-// const User = require('./models/user')
-
-// const main = async () => {
-// //  const task = await Task.findById('5eb2fe033b678f25f9c13fc2')
-// //  await task.populate('owner').execPopulate()
-// //  console.log(task.owner)
-
-// const user = await User.findById('5eb2f989f29b6124cdae3085')
-// await user.populate('tasks').execPopulate()
-// console.log(user.tasks)
-
-// }
-
-// main()
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const multer = require('multer')
+const sharp = require('sharp')
 const User = require('../models/user')
 const auth = require('../middleware/auth')
 
@@ -7,6 +9,43 @@ const router = new express.Router()
 //
 // User Routes
 //
+
+//Post Profile pic
+const upload = multer({
+ limits: {
+    fileSize: 1000000
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+      return cb(new Error('File format not supported (jpg, jpeg or png)'))
+    }
+    cb(undefined, true)
+  }
+})
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req, res) => { 
+  
+  const buffer = await sharp(req.file.buffer).resize({ width:250, height:250 }).png().toBuffer()
+  req.user.avatar = buffer
+  await req.user.save()
+
+  res.send()
+}, (error, req, res, next) => {
+  res.status(400).send({ error: error.message })
+})
+
+router.get('/users/:id/avatar', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+    if (!user || !user.avatar) {
+      throw new Error()
+    }
+
+    res.set('Content-Type', 'image/png')
+    res.send(user.avatar)
+  } catch (e) {
+    res.status(404).send()
+  }
+})
 
 //Create User
 router.post('/users', async (req, res) => {
@@ -82,6 +121,17 @@ router.patch('/users/me', auth, async (req, res) => {
     await req.user.save()
     res.send(req.user)
   } catch (e) { 
+    res.status(400).send(e)
+  }
+})
+
+//Delete
+router.delete('/users/me/avatar', auth, async (req, res) => {
+  req.user.avatar = undefined
+  try {
+    await req.user.save()
+    res.send()
+  } catch (e) {
     res.status(400).send(e)
   }
 })
